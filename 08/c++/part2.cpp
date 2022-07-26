785fcc010d16b7c93009cdcbb8d10db427922467
Ciaron Rider <ciaron@ridernator.com>
2022-07-26 22:45:18 +0100

    Added solution to day 8

---
+++ 08/c++/part2.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
+#define INPUT "../data/input"
+
+/**
+ * Read a file entirely into a vector of ints
+ * 
+ * @param The name of the file to read
+ * @return The vector of lines in the file
+ **/
+std::vector<uint32_t> readFileToVector(const std::string& filename = INPUT) {
+    std::ifstream fileStream(filename);
+    std::string string;
+    std::vector<uint32_t> returnVal;
+    
+    while (std::getline(fileStream, string, ' ')) {
+        returnVal.push_back(std::stol(string));
+    }
+
+    return returnVal;
+}
+
+typedef struct node {
+    uint32_t numSubNodes;
+    node** subNodes;
+
+    uint32_t numMetaData;
+    uint32_t* metaData;
+} Node;
+
+Node* createNode(const std::vector<uint32_t>& data,
+                 uint32_t& index) {
+    Node* node = new Node();
+
+    node->numSubNodes = data[index++];
+    node->numMetaData = data[index++];
+    node->subNodes = new Node*[node->numSubNodes];
+    node->metaData = new uint32_t[node->numMetaData];
+
+    // Create subnodes
+    for (uint32_t subNodeIndex = 0; subNodeIndex < node->numSubNodes; ++subNodeIndex) {
+        node->subNodes[subNodeIndex] = createNode(data, index);
+    }
+
+    // Create metadata
+    for (uint32_t metaDataIndex = 0; metaDataIndex < node->numMetaData; ++metaDataIndex) {
+        node->metaData[metaDataIndex] = data[index++];
+    }
+
+    return node;
+}
+
+void countMetaData(const Node* node,
+                   uint64_t& count) {
+    // Recursively count metadata in subnodes
+    // If no subnodes then add up metadata
+    if (node->numSubNodes == 0) {
+        for (uint32_t metaDataIndex = 0; metaDataIndex < node->numMetaData; ++metaDataIndex) {
+            count += node->metaData[metaDataIndex];
+        }
+    } else { // If subnodes
+        // Use metadata as indesx to subnodes to calculate
+        for (uint32_t metaDataIndex = 0; metaDataIndex < node->numMetaData; ++metaDataIndex) {
+            if ((node->metaData[metaDataIndex] > 0) &&
+                (node->metaData[metaDataIndex] <= node->numSubNodes)) {
+                countMetaData(node->subNodes[node->metaData[metaDataIndex] - 1], count);
+            }
+        }
+    }
+}
+
+void deleteNode(Node* node) {
+    delete [] node->metaData;
+
+    for (uint32_t subNodeIndex = 0; subNodeIndex < node->numSubNodes; ++subNodeIndex) {
+        deleteNode(node->subNodes[subNodeIndex]);
+    }
+
+    delete node;
+}
+
+int main() {
+    auto numbers = readFileToVector();
+
+    // Create all nodes
+    uint32_t index = 0;
+    Node* head = createNode(numbers, index);
+
+    // Count all metadata
+    uint64_t count = 0;
+    countMetaData(head, count);
+
+    std::cout << "Total of metadata is " << count << std::endl;
+
+    return 0;
+}
